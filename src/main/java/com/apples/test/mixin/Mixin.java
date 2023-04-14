11e7675ae8835a70a0c86bb83d49a5e00492ad0b
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-04-14 12:11:08 -0500

    Update and rename src/main/java/net/fabricmc/example/mixin/ExampleMixin.java to src/main/java/com/apples/test/mixin/Mixin.java

--- src/main/java/com/apples/test/mixin/Mixin.java
+++ src/main/java/com/apples/test/mixin/Mixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.apples.test.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.apples.test.TestMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class Mixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
 		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
