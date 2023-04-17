ee9cb07b8c5d1f9ce28dbec03a734d2eb5d94fac
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-04-17 08:40:24 -0500

    Update enchantments.java

--- src/main/java/com/apples/opitems/enchantments.java
+++ src/main/java/com/apples/opitems/enchantments.java
@@ -32,4 +32,74 @@ public class enchantments {
             super.onTargetDamaged(user, target, level);
         }
     }
+    
+    public static class FrostTipped extends Enchantment {
+        public FrostTipped() {
+            super(Enchantment.Rarity.UNCOMMON, EnchantmentTarget.WEAPON, new EquipmentSlot[] {EquipmentSlot.MAINHAND});
+        }
+        @Override
+        public int getMinPower(int level) {
+            return 1;
+        }
+
+        @Override
+        public int getMaxLevel() {
+            return 10;
+        }
+
+        @Override
+        public void onTargetDamaged(LivingEntity user, Entity target, int level) {
+            if(target instanceof LivingEntity) {
+                ((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 20 * 2 * level, level));
+            }
+
+            super.onTargetDamaged(user, target, level);
+        }
+    }
+    
+    public static class WitherTipped extends Enchantment {
+        public WitherTipped() {
+            super(Enchantment.Rarity.UNCOMMON, EnchantmentTarget.WEAPON, new EquipmentSlot[] {EquipmentSlot.MAINHAND});
+        }
+        @Override
+        public int getMinPower(int level) {
+            return 1;
+        }
+
+        @Override
+        public int getMaxLevel() {
+            return 10;
+        }
+
+        @Override
+        public void onTargetDamaged(LivingEntity user, Entity target, int level) {
+            if(target instanceof LivingEntity) {
+                ((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, 20 * 2 * level, level));
+            }
+
+            super.onTargetDamaged(user, target, level);
+        }
+    }
+    
+    /*public static class InvisHit extends Enchantment {
+        public InvisHit() {
+            super(Enchantment.Rarity.UNCOMMON, EnchantmentTarget.WEAPON, new EquipmentSlot[] {EquipmentSlot.MAINHAND});
+        }
+        @Override
+        public int getMinPower(int level) {
+            return 1;
+        }
+
+        @Override
+        public int getMaxLevel() {
+            return 1;
+        }
+
+        @Override
+        public void onTargetDamaged(LivingEntity user, Entity target, int level) {
+            user.addStatusEffect(
+
+            super.onTargetDamaged(user, target, level);
+        }
+    }*/
 }
